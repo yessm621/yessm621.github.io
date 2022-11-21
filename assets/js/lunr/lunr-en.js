e6e508083c620fc09baca6d91f5e49874b024449
yessm621 <yessm621@gmail.com>
2022-11-21 16:09:30 +0900

    검색창 이미지 삭제

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -49,9 +49,9 @@ $(document).ready(function() {
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="">'+
-              '</div>'+
+              // '<div class="archive__item-teaser">'+
+              //   '<img src="'+store[ref].teaser+'" alt="">'+
+              // '</div>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
           '</div>';
